17f8860dd370659ef23f6c0964c3c507cc7d992e
tooyoungtobeloved <1246755863@qq.com>
2020-06-04 10:36:05 +0800

    完成商品详情页和加入购物车功能

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -5,62 +5,60 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    goodsObj: {}
   },
-
+  goodsInfo:{},
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const {goods_id} = options;
+    this.getGoodsDetail(goods_id)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  getGoodsDetail(goods_id){
+    let that = this
+    wx.request({
+      url: "https://api-hmugo-web.itheima.net/api/public/v1/goods/detail",
+      data: {
+        goods_id
+      },
+      success: function(res){
+        const { message } = res.data;
+        that.goodsInfo = message;
+        that.setData({
+          goodsObj:{
+            goods_name:message.goods_name,
+            goods_price:message.goods_price,
+            goods_introduce:message.goods_introduce.replace(/\.webp/g,'.jpg'),
+            pics:message.pics
+          }
+        })
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  hanlePreviewImg(e){
+    const urls = this.goodsInfo.pics.map(i=>i.pics_mid)
+    const current =  e.currentTarget.dataset.url;
+    wx.previewImage({
+      current, // 当前显示图片的链接，不填则默认为 urls 的第一张
+      urls
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleCartAdd(){
+    // 获取本地存储的购物车数组
+    let cart = wx.getStorageSync("cart") || [];
+    let index = cart.findIndex(v => v.goods_id===this.goodsInfo.goods_id);
+    if(index === -1) {
+      this.goodsInfo.num = 1;
+      cart.push(this.goodsInfo)
+    }else {
+      cart[index].num++;
+    }
+    wx.setStorageSync("cart", cart);
+    wx.showToast({
+      title: "加入成功",
+      icon: "success",
+      mask: true
+    })
   }
 })
